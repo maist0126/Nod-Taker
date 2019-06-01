01194bb44f58aba33b118744f2a90522dbee71f7
maist <madong0126@gmail.com>
2019-06-01 18:34:52 +0900

    Intial

---
+++ index.js
@@ -0,0 +1,12 @@
+(function() {
+	var firebaseConfig = {
+		apiKey: "AIzaSyC2XbcwWxo3s23HjvPYD_dq94VyMjzh6lU",
+		authDomain: "mid-gradwork.firebaseapp.com",
+		databaseURL: "https://mid-gradwork.firebaseio.com",
+		projectId: "mid-gradwork",
+		storageBucket: "mid-gradwork.appspot.com",
+		messagingSenderId: "449677305537",
+		appId: "1:449677305537:web:d47f7aad8736334b"
+	};
+	firebase.initializeApp(firebaseConfig);
+}());
